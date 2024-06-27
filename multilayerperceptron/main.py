1592ee093386824183e088253f865d36c1ba49db
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-06-27 23:21:05 +0530

    feat: add datasets, dataloaders

--- multilayerperceptron/main.py
+++ multilayerperceptron/main.py
@@ -3,6 +3,7 @@ import matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
 
 df = pd.read_csv("xor_dataset.csv")
 X = df[["x1", "x2"]].values
@@ -34,4 +35,24 @@ class MultiLayerPerceptron(torch.nn.Module):
         logits = self.all_layers(x)
         return logits
 
+class MLPDataSet(Dataset):
+    def __init__(self, X, y):
+        self.features = torch.tensor(X, dtype=torch.float32)
+        self.labels = torch.tensor(y, dtype=torch.int64)
+
+    def __getitem__(self, index):
+        x = self.features[index]
+        y = self.labels[index]
+
+        return x, y
     
+    def __len__(self):
+        return self.labels.shape[0]
+    
+train_dataset = MLPDataSet(X_train, y_train)
+val_dataset = MLPDataSet(X_val, y_val)
+test_dataset = MLPDataSet(X_test, y_test)
+
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
