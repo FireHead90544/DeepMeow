aedd87a4ef44ed9b4d862b7ad7941f2a5863f01e
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-21 02:02:56 +0530

    feat: create mapping between str - num vals

---
+++ star_classification/maps.py
@@ -0,0 +1,32 @@
+spectral_classes = {"O": 0, "B": 1, "A": 2, "F": 3, "G": 4, "K": 5, "M": 6}
+
+star_categories = {
+    0: "Brown Dwarf",
+    1: "Red Dwarf",
+    2: "White Dwarf",
+    3: "Main Sequence",
+    4: "Supergiant",
+    5: "Hypergiant",
+}
+
+star_colors = {
+    "Red": 0,
+    "Blue White": 1,
+    "White": 2,
+    "Yellowish White": 3,
+    "Blue white": 4,
+    "Pale yellow orange": 5,
+    "Blue": 6,
+    "Blue-white": 7,
+    "Whitish": 8,
+    "yellow-white": 9,
+    "Orange": 10,
+    "White-Yellow": 11,
+    "white": 12,
+    "Blue ": 13,
+    "yellowish": 14,
+    "Yellowish": 15,
+    "Orange-Red": 16,
+    "Blue white ": 17,
+    "Blue-White": 18,
+}
